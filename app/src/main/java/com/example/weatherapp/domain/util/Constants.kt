c9d6f210df3787ee87744191bc5211d8cd75286b
DESKTOP-JE20NTR\Keneki <alipunjabian659@gmail.com>
2022-09-07 15:40:40 +0500

    New API key with 60 calls/min to get weather details

--- app/src/main/java/com/example/weatherapp/domain/util/Constants.kt
+++ app/src/main/java/com/example/weatherapp/domain/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val UNITS = "metric"
         const val LANGUAGE = "en"
-        const val APPID = "107d5d66032d410e7af1df8271cc6098"
+        const val APPID = "d3a916d4b661fe798ae41b25ed5a150f"
         const val CONNECTION_TIMEOUT_MS: Long = 60
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     }
